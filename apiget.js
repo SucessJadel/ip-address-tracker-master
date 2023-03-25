1ff9aa3f3c26376a095b7140465fffc017140615
SucessJadel <successjadel@gmail.com>
2023-03-26 00:46:48 +0100

    added content of leaflet.js to app.js to set the lat and longitude as a variable

--- apiget.js
+++ apiget.js
@@ -1,5 +1,3 @@
-console.log("hi");
-
 class Apiip {
   constructor() {
     this.apiKey = "00e8d50b7d19050e39c9b36147ced28505f4fc889284cd54d1a855fa";
@@ -15,6 +13,3 @@ class Apiip {
     return responseData;
   }
 }
-// json().then((data) => {
-//   console.log(data.ip);
-// });
--- app.js
+++ app.js
@@ -7,6 +7,18 @@ document.addEventListener("DOMContentLoaded", getIpAddress);
 function getIpAddress() {
   apiip
     .getIp()
-    .then((res) => console.log(res))
+    .then((res) => {
+      const lat = res.latitude;
+      const lng = res.longitude;
+      var map = L.map("map").setView([lat, lng], 13);
+      var marker = L.marker([lat, lng]).addTo(map);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+      console.log(res);
+    })
     .catch((err) => console.log(err));
 }
